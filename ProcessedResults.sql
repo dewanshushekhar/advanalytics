4127580394982d9b692412a95d2d0779c67e489a
dewanshushekhar <51441037+dewanshushekhar@users.noreply.github.com>
2019-06-07 11:05:04 +0530

    Add files via upload

---
+++ ProcessedResults.sql
@@ -0,0 +1,11 @@
+Create Table ProcessedResults (
+	CustomerId INT,
+	FullName NVARCHAR(100),
+	CreditLimit INT,
+	ActiveStatus BIT,
+	City NATIONAL CHARACTER VARYING(50),
+	State NVARCHAR(50),
+	Country NVARCHAR(50),
+	SaleAmount INT,
+	NoOfUnits INT
+)
